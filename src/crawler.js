21a03941f02d8491a4c06cc81ecc8f438a26f9ba
prynssmaia <prynss_lp@hotmail.com>
2022-03-04 17:13:53 -0300

    add file system module

--- src/crawler.js
+++ src/crawler.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://example.com/');
+    await page.goto('http://producao.geomais.com.br/');
     await page.screenshot({ path: 'example.png' });
   
-    await browser.close();
+    // await browser.close();
   })();
