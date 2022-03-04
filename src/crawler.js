1f41f2a6cf7cc3eb3ee99b61be12ab831d848784
prynssmaia <prynss_lp@hotmail.com>
2022-03-04 17:33:16 -0300

    add crawling version number

--- src/crawler.js
+++ src/crawler.js
@@ -4,8 +4,36 @@ const fs = require('fs');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://producao.geomais.com.br/');
-    await page.screenshot({ path: 'example.png' });
+    await page.goto('http://producao.geomais.com.br/changelog.html');
+    // await page.screenshot({ path: 'example.png' });
+
+    const versaoList = await page.evaluate( () => {
+
+      const nodeList = document.querySelectorAll('.content')
+      const dataArray = [...nodeList]
+
+      // transformar os elementos html em objeto js
+      const versaoList = dataArray.map(function(elemento, indice, arrayBase) {
+
+        // buscando número da versão
+        const versaoBase = elemento.querySelector('.content h2:nth-of-type(2)').textContent
+        const versaoSlice = versaoBase.slice(0,6)
+        const versao = versaoSlice
+
+        return { versao: versao}
+
+      })
+
+      return versaoList 
+
+    })
+
+  // escrevendo os dados em um arquivo local (json)
+  fs.writeFile('versao_data.json', JSON.stringify(versaoList, null, 2), err => {
+    if(err) throw new Error('Something went wrong')
+
+    console.log('Well done!')
+  })
   
-    // await browser.close();
+    await browser.close();
   })();
