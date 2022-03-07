be9be2b02a44bc903891a45c3a50331d39d8ed0a
prynssmaia <prynss_lp@hotmail.com>
2022-03-07 17:50:37 -0300

    minor changes

--- src/crawler.js
+++ src/crawler.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://producao.geomais.com.br/changelog.html');
+    await page.goto('https://geo.bc.sc.gov.br/changelog.html');
     // await page.screenshot({ path: 'example.png' });
 
     const versaoList = await page.evaluate( () => {
