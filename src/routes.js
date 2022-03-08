6bdf6d9b4731cdd3dd478850568d868790a0a59a
prynssmaia <prynss_lp@hotmail.com>
2022-03-08 17:43:04 -0300

    tests changes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const routes = express.Router()
+const path = require('path')
+const versaodata = require('../versao_data.json')
+
+const views = path.join(__dirname, '/views/')
+
 
 
 routes.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile('index.html')
   })
 
 module.exports = routes
