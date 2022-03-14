e13bc8d041ccf228f9e8b1d0c5aa7cdc62fa9ff2
Prynss <prynss_lp@hotmail.com>
2022-03-13 21:32:38 -0300

    add getting version from two pages

--- src/crawler.js
+++ src/crawler.js
@@ -2,54 +2,42 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const urls = ['https://google.com','https://youtube.com']
+  const urls = ['http://producao.geomais.com.br/changelog.html', 'http://demo.geomais.com.br/changelog.html']
+  const fullxpath = '/html/body/div/div/div[2]/div[2]/a/text()[2]'
+  const seletor = '#root > div > div.ant-col.sc-iJminA.kqnHRJ.ant-col-xs-24.ant-col-lg-10.ant-col-xl-8 > div.public-access > a'
+
   
   for ( let i = 0; i < urls.length; i++) {
     const url = urls[i]
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch( { headless: false } )
     const page = await browser.newPage()
     await page.goto(`${url}`)
     await page.screenshot({ path: `example${i}.png` });
-    await browser.close();
-  }
 
+    const versao = await page.evaluate( () => { 
+      const nodeList = document.querySelectorAll('.content')
+
+      const versionArray = [...nodeList]
+
+      const versaoList = versionArray.map(function(elemento, indice, arrayBase) {
+
+        const versionBase = elemento.querySelector('.content h2:nth-of-type(2)').textContent
+        const versaoSlice = versionBase.slice(0, 6)
+        const version = versaoSlice
+
+        return version
+      })
+
+      return versaoList
+    })
+    console.log(versao)
+
+    await browser.close();
+
+  }
     
   })();
-
-// xPath para o número de versão
-// $x('/html/body/div/div/div[2]/div[2]/a/text()[2]')[0].wholeText
-
-
-
-  //   const browser = await puppeteer.launch();
-
-  //   const geo = 'https://geo.bc.sc.gov.br/changelog.html'
-
-  //   const page = await browser.newPage();
-
-  //   await page.goto(geo);
-
-  //   const versaoList = await page.evaluate( () => {
-
-  //     const nodeList = document.querySelectorAll('.content')
-  //     const dataArray = [...nodeList]
-
-  //     // transformar os elementos html em objeto js
-  //     const versaoList = dataArray.map(function(elemento, indice, arrayBase) {
-
-  //       // buscando número da versão
-  //       const versaoBase = elemento.querySelector('.content h2:nth-of-type(2)').textContent
-  //       const versaoSlice = versaoBase.slice(0,6)
-  //       const versao = versaoSlice
-
-  //       return { versao: versao }
-
-  //     })
-
-  //     return versaoList 
-
-  //   })
-
+  
   // //escrevendo os dados em um arquivo local (json)
   // fs.writeFile('versao_data.json', JSON.stringify(versaoList, null, 2), err => {
   //   if(err) throw new Error('Something went wrong')
