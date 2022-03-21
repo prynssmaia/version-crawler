d80760972c8bb05e8379ba3f5f7f28b0d6075b21
prynssmaia <prynss_lp@hotmail.com>
2022-03-21 13:24:43 -0300

    fix import of json file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const port = 3000
+const versao_data = require('../versao_data.json')
 
 const server = express()
 server.set('view engine', 'ejs')
@@ -9,7 +10,7 @@ server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
 server.use(routes)
 
-server.locals.versao_data = require('../versao_data.json')
+server.locals.versao_data = versao_data
 
 server.listen(port, () => console.log(`Rodando em http://localhost:${port}`))
 
