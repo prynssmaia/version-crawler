3719d2d3564e3f4f881db3dc307b57d4a0760a64
prynssmaia <prynss_lp@hotmail.com>
2022-03-21 13:24:08 -0300

    fix remove unnecessary information

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const routes = express.Router()
 const path = require('path')
-const versao_data = require('../versao_data.json')
-const ejs = require('ejs')
 
 
 const views = path.join(__dirname, '/views/')
@@ -11,14 +9,6 @@ const views = path.join(__dirname, '/views/')
 
 routes.get('/', (req, res) => {
     res.render(views + 'index')
-
-    // ejs.renderFile(path.join(__dirname, 'views', 'index.ejs'), { versao_data },function (err, data) {
-    //   if (err) {
-    //     console.log(err)
-    //   } else {
-    //     console.log(data)
-    //   }
-    // })
   })
 
 module.exports = routes
