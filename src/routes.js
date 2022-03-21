b4274bb9f48fd61cc5fc7eef978bae3b41aafaf1
Prynss <prynss_lp@hotmail.com>
2022-03-20 21:33:32 -0300

    add ejs and json import for some tests

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 const path = require('path')
+const versao_data = require('../versao_data.json')
+const ejs = require('ejs')
+
 
 const views = path.join(__dirname, '/views/')
 
@@ -8,6 +11,14 @@ const views = path.join(__dirname, '/views/')
 
 routes.get('/', (req, res) => {
     res.render(views + 'index')
+
+    // ejs.renderFile(path.join(__dirname, 'views', 'index.ejs'), { versao_data },function (err, data) {
+    //   if (err) {
+    //     console.log(err)
+    //   } else {
+    //     console.log(data)
+    //   }
+    // })
   })
 
 module.exports = routes
