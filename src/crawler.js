33a65ef7e9959a7be70f600062228f9de18d6b7b
Prynss <prynss_lp@hotmail.com>
2022-03-28 21:20:00 -0300

    add: list of countys

--- src/crawler.js
+++ src/crawler.js
@@ -2,8 +2,23 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 async function getVersion() {
-  //const urls = ['http://producao.geomais.com.br/changelog.html',http://demo.geomais.com.br/changelog.html']
-  const urls = ['https://geo.bc.sc.gov.br/changelog.html', 'http://201.59.100.253:3080/changelog.html', 'http://187.87.208.107:3390/changelog.html']
+  //const urls = ['http://producao.geomais.com.br/changelog.html','http://demo.geomais.com.br']  
+  //const urls = ['https://geo.bc.sc.gov.br/changelog.html', 'http://201.59.100.253:3080/changelog.html', 'http://187.87.208.107:3390/changelog.html']
+  const urls = [
+    'http://geo.alfredochaves.es.gov.br/changelog.html',
+    'https://geo.arapongas.pr.gov.br/changelog.html',
+    'https://geo.bc.sc.gov.br/changelog.html',
+    'http://geo.bigua.sc.gov.br/changelog.html',
+    'http://geomais.camacari.ba.gov.br/changelog.html',
+    'http://geo.ibirama.sc.gov.br:8082/changelog.html',
+    'http://geo.itapema.sc.gov.br:8091/changelog.html',
+    'http://162.214.203.123:8081/changelog.html',
+    'http://geo.navegantes.sc.gov.br/changelog.html',
+    'http://201.59.100.253:3080/changelog.html',
+    'http://177.85.24.243:25480/changelog.html',
+    'https://geo.timbo.sc.gov.br/changelog.html',
+    'http://187.87.208.107:3390/changelog.html'
+  ]
 
   const browser = await puppeteer.launch( { headless: true } )
   const page = await browser.newPage()
